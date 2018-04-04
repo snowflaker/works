66d1766c98f1185e048faa66d8975e529c2cd5a6
snowflaker <test22773@gmail.com>
2018-04-04 21:16:13 +0300

    Exercise 1 from the file Homework

---
+++ hw_1.py
@@ -0,0 +1,16 @@
+# exercise1_1
+print("57854".isdigit())
+
+# exercise1_2
+print("   oujiojewf  8".count(" "))
+
+# exercise1_3
+print("example.1, example.2.".count("."))
+
+# exercise1_4
+short_string = "Homework".center(100, " ")
+print(short_string)
+print(len(short_string))
+
+# exercise1_5
+print("hello a big world".title())
