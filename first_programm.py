71836be9561c5a6b6ca1bac7304b37ce1167cf84
snowflaker <test22773@gmail.com>
2018-04-06 20:54:35 +0300

    delete

--- first_programm.py
+++
@@ -1,9 +0,0 @@
-user_string = input("Enter please: ")
-user_string = str(user_string)
-user_feedback_hi = "Goodbye"
-user_feedback_else = "Tell me: Hi"
-
-if user_string == "Hi":
-    print(user_feedback_hi)
-else:
-    print(user_feedback_else)
