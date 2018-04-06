6a0eaf27e2ae4b8550bb52ebf1e83e36dc887d12
snowflaker <test22773@gmail.com>
2018-04-06 20:44:51 +0300

    added a description to the tasks

--- hw_1.py
+++ hw_1.py
@@ -1,16 +1,16 @@
-# exercise1_1
+# exercise1_1 The string is a record of a number
 print("57854".isdigit())
 
-# exercise1_2
+# exercise1_2 How many spaces in the string
 print("   oujiojewf  8".count(" "))
 
-# exercise1_3
+# exercise1_3 How many symbol '.' in the string
 print("example.1, example.2.".count("."))
 
-# exercise1_4
+# exercise1_4 The string in the middle
 short_string = "Homework".center(100, " ")
 print(short_string)
 print(len(short_string))
 
-# exercise1_5
+# exercise1_5 First letters of the words in uppercase
 print("hello a big world".title())
---
+++ hw_2.py
@@ -0,0 +1,10 @@
+# exercise2_1
+from math import sqrt
+
+side_1 = 179
+side_2 = 971
+
+side_3 = sqrt((side_1**2) + (side_2**2))
+print(int(side_3))
+
+# exercise2_2
